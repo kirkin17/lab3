db00c0d1244e3a0a8cc6a4be88f043fb18b1f70f
kirkin17 <kirkin17@users.noreply.github.com>
2019-12-05 00:26:42 +0300

    v1

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int n, i, j, m = 10;
+	cin >> n;
+	cout << "1\t2\t";
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		for (j = 3; j <= n; j += 2)
+		{
+			if (i%j == 0)
+			{
+				for (i; i <= m; m += 10)
+				{
+					cout << i << "\t";
+				}
+			}
+		}
+	}
+}
