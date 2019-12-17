1e2b16a1f98efe980b2f56adda605b4e18d87abd
kirkin17 <kirkin17@users.noreply.github.com>
2019-12-17 21:23:09 +0300

    v2

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -3,20 +3,32 @@
 using namespace std;
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	int n, i, j, m = 10;
+	bool flag = false;
+
+	cout << "Введите N: ";
 	cin >> n;
-	cout << "1\t2\t";
-	for (i = 3; i <= sqrt(n); i += 2)
+//	cout << "1\t2\t";
+	for (i = 1; i <= 2; i++)							//проверяем исключения 1 и 2 (бессмысленно)
 	{
-		for (j = 3; j <= n; j += 2)
+		for (j = 1; j <= 2; j++)
+		{
+			if (i % j == 0) { flag = true; };
+		}
+		cout << i << "\t";
+	}
+
+	for (i = 3; i <= n; i += 2)							//проверяем остальное до N
+	{
+		if (i % 2 == 0) continue;
+		for (j = 1; j <= sqrt(n); j += 2)
 		{
 			if (i%j == 0)
 			{
-				for (i; i <= m; m += 10)
-				{
-					cout << i << "\t";
-				}
+				flag = true;
 			}
 		}
-	}
+		if (flag) { cout << i << "\t"; };
+	}													//
 }
